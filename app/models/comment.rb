a1b9e4a1d3adacc68de4f7409c77f8b24cffab78
WilhelmK109 <kamulungaw@gmail.com>
2023-08-17 12:30:03 +0200

    Create custom methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, foreign_key: 'author_id', class_name: 'User'
   belongs_to :post
+
+  after_create :update_comments_counter
+
+  def update_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,10 @@
 class Like < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, foreign_key: 'author_id', class_name: 'User'
   belongs_to :post
+
+  after_create :update_likes_counter
+
+  def update_likes_counter
+    post.update(likes_counter: post.likes.count)
+  end
 end
