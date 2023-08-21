7be226e23dc6c6a6cdf3a7cf4113064c4e1974f6
WilhelmK109 <kamulungaw@gmail.com>
2023-08-21 16:36:04 +0200

    Create the controllers manually

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  def index
+    # Placeholder for users index page
+    # render plain: "Here is a list of users"
+  end
+
+  def show
+    # Placeholder for user show page
+    # render plain: "Here is the profile of a user"
+  end
+end
