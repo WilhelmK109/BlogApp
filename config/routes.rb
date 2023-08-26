fb14a7c9fd9fdc360fe613ce275aa51f5be5459b
WilhelmK109 <kamulungaw@gmail.com>
2023-08-26 14:39:10 +0200

    Create forms to add posts and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  # root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :new, :show] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+    end
   end
+  post "users/:user_id/posts", to: "post#create", as: :user_create_post
 end
