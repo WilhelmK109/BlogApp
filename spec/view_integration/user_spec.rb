ddd963833ec83627ff12feef76be7c64a995c702
WilhelmK109 <kamulungaw@gmail.com>
2023-08-29 19:02:54 +0200

    Fix linter errors

--- spec/view_integration/user_spec.rb
+++ spec/view_integration/user_spec.rb
@@ -1,5 +1,5 @@
-
 require 'rails_helper'
+
 RSpec.describe 'User Workflow', type: :feature do
   let!(:user) { create(:user, name: 'Chikachika', posts_counter: 1, photo: 'http://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Ethiopia') }
   let!(:user2) { create(:user, name: 'Pelepele', posts_counter: 1, photo: 'http://unsplash.com/photos/F_-0BxGuVvo', bio: 'Cook from Zambia') }
@@ -10,12 +10,14 @@ RSpec.describe 'User Workflow', type: :feature do
       expect(page).to have_selector("img[src='#{user.photo}']")
       expect(page).to have_content(user.posts_counter)
     end
+
     it 'redirects to user show page when clicking on a user' do
       visit root_path(user)
       click_link("user-name-#{user2.id}")
       expect(page).to have_current_path("/users/#{user2.id}")
     end
   end
+
   describe 'User show page' do
     it 'displays user information' do
       visit user_path(user)
@@ -23,17 +25,20 @@ RSpec.describe 'User Workflow', type: :feature do
       expect(page).to have_selector("img[src='#{user.photo}']")
       expect(page).to have_content(user.bio)
     end
+
     it 'displays the user\'s first 3 posts' do
       posts = create_list(:post, 3, author: user)
       visit user_path(user)
       posts.each { |post| expect(page).to have_content(post.title) }
     end
+
     it 'redirects to a post show page when clicking on a user\'s post' do
       post = create(:post, author: user)
       visit user_path(user)
       click_link(post.title)
       expect(page).to have_current_path(user_post_path(user, post.id))
     end
+
     it 'redirects to user post index page when clicking "View All Posts"' do
       visit user_path(user)
       click_link('See all posts')
