a86787f4af907a5a722e4feb966d8fba5b636f82
WilhelmK109 <kamulungaw@gmail.com>
2023-08-30 18:50:51 +0200

    Install and setup Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-  helper_method :current_user
-
-  def current_user
-    @current_user ||= User.first
-  end
+  
 end
