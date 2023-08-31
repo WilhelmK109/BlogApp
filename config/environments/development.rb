19b35e5afc016d08fa39ab425a668b3eb52eb28e
Enning94 <enningfestus@gmail.com>
2023-08-31 16:53:18 +0000

    Update database.yml file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,4 +69,13 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :user_name => '0f64cfe34da430',
+  :password => '8b449a7d49ac51',
+  :address => 'sandbox.smtp.mailtrap.io',
+  :host => 'sandbox.smtp.mailtrap.io',
+  :port => '2525',
+  :authentication => :cram_md5
+}
 end
