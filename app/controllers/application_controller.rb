f9e5e15eadab9021146cbb5429b153d80b335375
Enning94 <enningfestus@gmail.com>
2023-08-31 16:51:41 +0000

    modified code to register new user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
   
-end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+        user_params.permit(:name, :photo, :bio, :email, :password, :password_confirmation)
+      end
+    end
+  end
+  
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  validates :name, presence: true
+  # validates :name, presence: true
 
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
